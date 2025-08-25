3704a945188cc3c55f3e1e94b5a4e5e127d724e4
v_zhoulinfeng <v_zhoulinfeng@baidu.com>
2025-08-25 10:36:34 +0800

    feat: 初始化eslint 配置

---
+++ apps/web/app/pages/category/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="p-6 lg:px-8 ">
+    <section class="bg-[#08396e] h-[16rem] rounded-md" />
+    <section class="flex font-semibold pt-4">
+      <section class="flex-auto">
+        <header class="py-2">最近内容</header>
+        <ul class="flex gap-2 flex-wrap">
+          <li class="basis-[calc(50%-0.5rem)] h-[10rem] rounded-md bg-[#083a6d]" />
+          <li class="basis-[calc(50%-0.5rem)] h-[10rem] rounded-md bg-[#083a6d]" />
+          <li class="basis-[calc(50%-0.5rem)] h-[10rem] rounded-md bg-[#083a6d]" />
+          <li class="basis-[calc(50%-0.5rem)] h-[10rem] rounded-md bg-[#083a6d]" />
+        </ul>
+      </section>
+      <section class="w-1/3 flex flex-col">
+        <header class="py-2">全部</header>
+        <section class="flex-grow">
+          <ul class="bg-[#FFCE63] h-full rounded-md">
+            <li class="p-2">设计</li>
+            <li class="p-2">设计</li>
+            <li class="p-2">设计</li>
+            <li class="p-2">设计</li>
+          </ul>
+        </section>
+      </section>
+    </section>
+  </section>
+</template>
